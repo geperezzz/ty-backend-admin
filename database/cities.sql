3c8006b74caff6ff079dbee499a778f523d64b70
Gabriel Pérez <gph393@gmail.com>
2023-07-10 22:28:06 -0400

    Make cities a weak entity
    As cities depend on which state they are located at, make `state_id` part of the primary key of the `cities` table and update all references to it

--- database/cities.sql
+++ database/cities.sql
@@ -1,11 +1,13 @@
 CREATE TABLE cities (
     CONSTRAINT cities_pk
-        PRIMARY KEY (id),
+        PRIMARY KEY (city_number, state_id),
     CONSTRAINT cities_state_id_fk
         FOREIGN KEY (state_id) REFERENCES states (id)
             ON UPDATE CASCADE
             ON DELETE RESTRICT,
-    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    city_number INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
     name TEXT NOT NULL,
-    state_id INTEGER NOT NULL
+    state_id INTEGER NOT NULL,
+    CONSTRAINT unique_name_state_id
+        UNIQUE (name, state_id)
 );
--- database/dealerships.sql
+++ database/dealerships.sql
@@ -2,7 +2,7 @@ CREATE TABLE dealerships (
     CONSTRAINT dealerships_pk
         PRIMARY KEY (rif),
     CONSTRAINT dealerships_city_id_fk
-        FOREIGN KEY (city_id) REFERENCES cities (id)
+        FOREIGN KEY (city_number, state_id) REFERENCES cities (city_number, state_id)
             ON UPDATE CASCADE
             ON DELETE RESTRICT,
     CONSTRAINT dealerships_manager_national_id_fk
@@ -11,6 +11,7 @@ CREATE TABLE dealerships (
             ON DELETE RESTRICT,
     rif rif NOT NULL,
     name TEXT NOT NULL,
-    city_id INTEGER NOT NULL,
+    city_number INTEGER NOT NULL,
+    state_id INTEGER NOT NULL,
     manager_national_id national_id NOT NULL
 );
