eca11450d668b78d11612b53afdde09b7d76cd7f
Gabriel Pérez <gph393@gmail.com>
2023-07-13 23:58:15 -0400

    Add pagination for city model

--- src/models/city.rs
+++ src/models/city.rs
@@ -1,21 +1,22 @@
-use sqlx::{
-    Executor,
-    Postgres
-};
-use serde::{
-    Serialize,
-    Deserialize
-};
+use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
+use sqlx::{Executor, Postgres};
+
+use crate::utils::pagination::{Page, Pages, Paginable};
 
 #[derive(Serialize, Deserialize)]
 pub struct City {
     pub city_number: i32,
     pub name: String,
-    pub state_id: i32
+    pub state_id: i32,
 }
 
 impl City {
-    pub async fn select(city_number: i32, state_id: i32, connection: impl Executor<'_, Database = Postgres>) -> Result<City, sqlx::Error> {
+    pub async fn select(
+        city_number: i32,
+        state_id: i32,
+        connection: impl Executor<'_, Database = Postgres>,
+    ) -> Result<City, sqlx::Error> {
         sqlx::query_as!(
             City,
             r#"
@@ -32,7 +33,9 @@ impl City {
         .await
     }
 
-    pub async fn select_all(connection: impl Executor<'_, Database = Postgres>) -> Result<Vec<City>, sqlx::Error> {
+    pub async fn select_all(
+        connection: impl Executor<'_, Database = Postgres>,
+    ) -> Result<Vec<City>, sqlx::Error> {
         sqlx::query_as!(
             City,
             r#"
@@ -44,7 +47,11 @@ impl City {
         .await
     }
 
-    pub async fn delete(city_number: i32, state_id: i32, connection: impl Executor<'_, Database = Postgres>) -> Result<City, sqlx::Error> {
+    pub async fn delete(
+        city_number: i32,
+        state_id: i32,
+        connection: impl Executor<'_, Database = Postgres>,
+    ) -> Result<City, sqlx::Error> {
         sqlx::query_as!(
             City,
             r#"
@@ -62,14 +69,45 @@ impl City {
     }
 }
 
+#[async_trait]
+impl Paginable<City> for City {
+    async fn get_page(
+        pages: &Pages<City, City>,
+        page_no: i64,
+        connection: impl Executor<'_, Database = Postgres>,
+    ) -> Result<Page<City>, sqlx::Error> {
+        let page_items = sqlx::query_as!(
+            City,
+            r#"
+                SELECT city_number, name, state_id
+                FROM cities
+                LIMIT $1
+                OFFSET $2
+            "#,
+            pages.per_page,
+            page_no * pages.per_page
+        )
+        .fetch_all(connection)
+        .await?;
+
+        Ok(Page {
+            per_page: pages.per_page,
+            items: page_items,
+        })
+    }
+}
+
 #[derive(Serialize, Deserialize)]
 pub struct InsertCity {
     pub name: String,
-    pub state_id: i32
+    pub state_id: i32,
 }
 
 impl InsertCity {
-    pub async fn insert(self, connection: impl Executor<'_, Database = Postgres>) -> Result<City, sqlx::Error> {
+    pub async fn insert(
+        self,
+        connection: impl Executor<'_, Database = Postgres>,
+    ) -> Result<City, sqlx::Error> {
         sqlx::query_as!(
             City,
             r#"
@@ -88,11 +126,15 @@ impl InsertCity {
 #[derive(Serialize, Deserialize)]
 pub struct UpdateCity {
     pub name: Option<String>,
-    pub state_id: Option<i32>
+    pub state_id: Option<i32>,
 }
 
 impl UpdateCity {
-    pub async fn update(self, target: City, connection: impl Executor<'_, Database = Postgres>) -> Result<City, sqlx::Error> {
+    pub async fn update(
+        self,
+        target: City,
+        connection: impl Executor<'_, Database = Postgres>,
+    ) -> Result<City, sqlx::Error> {
         let new_name = self.name.unwrap_or(target.name);
         let new_state_id = self.state_id.unwrap_or(target.state_id);
 
