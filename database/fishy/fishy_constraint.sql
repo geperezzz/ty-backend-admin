91e2682d1ecf21003ebe114aabe7eb86b5f842d2
Daikone <eduardoarzolay@gmail.com>
2023-07-21 22:17:41 -0400

    Deleted fishy constraints, yay :D

--- database/fishy/fishy_constraint.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE dealerships
-    ADD CONSTRAINT dealerships_manager_national_id_fk
-        FOREIGN KEY (manager_national_id) REFERENCES staff (national_id)
-            ON UPDATE CASCADE
-            ON DELETE RESTRICT;
